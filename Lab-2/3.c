dfea19538e1266f873364f819ced3be961359ed0
Tonnam eiei <tonnamerak@gmail.com>
2025-12-21 19:31:39 +0700

    Update 3.c

--- Lab-2/3.c
+++ Lab-2/3.c
@@ -1,1 +1,24 @@
+#include <stdio.h>
 
+int main() {
+    int N, i;
+    float unitPrice;
+    float discount, totalDiscount = 0.0;
+
+    if (scanf("%d", &N) != 1) {
+        return 1;
+    }
+
+    for (i = 0; i < N; i++) {
+        if (scanf("%f", &unitPrice) != 1) {
+            return 1;
+        }
+
+        discount = unitPrice * 0.04;
+        totalDiscount += discount;
+    }
+
+    printf("%.2f\n", totalDiscount);
+
+    return 0;
+}
