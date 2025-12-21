07ae5f214930ba2a165a465a852ed4fc7b6cb501
Tonnam eiei <tonnamerak@gmail.com>
2025-12-21 16:16:38 +0700

    Update L7.c

--- Lab-1/L7.c
+++ Lab-1/L7.c
@@ -1,45 +1,33 @@
 #include <stdio.h>
 
-int main()
-{
+int main() {
     int customerType;
-    double consumption_kwh, totalBill;
+    float consumption_kWh;
+    float totalBill = 0.0;
 
-    printf("Enter customer type (1 or 2): ");
-    scanf("%d", &customerType);
-
-    printf("Enter consumption (kWh): ");
-    scanf("%lf", &consumption_kwh);
-
-    if (customerType == 1)
-    {
-        if (consumption_kwh <= 100)
-        {
-            totalBill = consumption_kwh * 3.0;
-        }
-        else
-        {
-            totalBill = consumption_kwh * 4.0;
-        }
-    }
-    else if (customerType == 2)
-    {
-        if (consumption_kwh <= 500)
-        {
-            totalBill = consumption_kwh * 5.0;
-        }
-        else
-        {
-            totalBill = consumption_kwh * 6.5;
-        }
-    }
-    else
-    {
-        printf("Invalid Customer Type\n");
+    if (scanf("%d %f", &customerType, &consumption_kWh) != 2) {
         return 1;
     }
 
-    printf("Total Bill: %.2f\n", totalBill);
-  
+    if (customerType == 1) {  
+        if (consumption_kWh <= 100) {
+            totalBill = consumption_kWh * 3.0;
+        } else {
+            totalBill = consumption_kWh * 4.0;
+        }
+
+    } else if (customerType == 2) {  
+        if (consumption_kWh <= 500) {
+            totalBill = consumption_kWh * 5.0;
+        } else {
+            totalBill = consumption_kWh * 6.5;
+        }
+
+    } else {
+        printf("Invalid Customer Type\n");
+        return 0;
+    }
+
+    printf("%.2f\n", totalBill);
     return 0;
 }
