91d259afccc024f3cf3663fef24e7a6a7c71e7c3
Tonnam eiei <tonnamerak@gmail.com>
2025-12-21 16:07:39 +0700

    Update L6.c

--- Lab-1/L6.c
+++ Lab-1/L6.c
@@ -1,26 +1,20 @@
 #include <stdio.h>
 
-int main()
-{
+int main() {
     int number;
 
-    do
-    {
+    do {
         printf("Enter a number (1-10): ");
-      
-        if (scanf("%d", &number) != 1)
-        {
-            printf("Error: Value must be 1-10.\n");
-            while (getchar() != '\n')
-                ;
+        if (scanf("%d", &number) != 1) {
+            return 1;
         }
-        else if (number < 1 || number > 10)
-        {
+
+        if (number < 1 || number > 10) {
             printf("Error: Value must be 1-10.\n");
         }
+
     } while (number < 1 || number > 10);
 
     printf("Input accepted: %d\n", number);
-  
     return 0;
 }
